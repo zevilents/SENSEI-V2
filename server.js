137165e0c5895c3812c70b30755b634a891d693f
zevilents <132818986+zevilents@users.noreply.github.com>
2026-03-12 20:53:12 +0700

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.post('/api/chat', async (req, res) => {
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
-        "model": "nvidia/nemotron-3-nano-30b-a3b:free",
+        "model": "openrouter/hunter-alpha",
         "messages": messages,
         // Karena kamu bilang "gak mau reasoning", kita set false di sini
         "reasoning": { "enabled": false } 
